76380e3f790316e2f757e3daec2f14da4759f249
Derek Walz <derek.walz@ucalgary.ca>
2023-04-28 11:20:23 -0600

    use NgSrc to improve image rendering

--- client/src/app/modules/recipeSearch/recipeSearch.module.ts
+++ client/src/app/modules/recipeSearch/recipeSearch.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RecipeSearchComponent } from "./recipeSearch.component";
-import { CommonModule } from "@angular/common";
+import { CommonModule, NgOptimizedImage } from "@angular/common";
 import { InputTextModule } from "primeng/inputtext";
 import { FormsModule } from "@angular/forms";
 import { ButtonModule } from "primeng/button";
@@ -18,6 +18,7 @@ import { AccordionModule } from 'primeng/accordion';
         DataViewModule,
         FormsModule,
         InputTextModule,
+        NgOptimizedImage,
         RippleModule
     ],
     exports: [ RecipeSearchComponent ],
